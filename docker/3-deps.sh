cb46374ea4f2db4f3a6cc93145219accf906f918
Alexander Rössler <mail@roessler.systems>
2019-08-20 10:56:51 +0200

    add mycroft log directory

--- docker/3-deps.sh
+++ docker/3-deps.sh
@@ -11,6 +11,9 @@ apt-get update
 
 apt-get install -y git python3 python3-dev python3-setuptools libtool libffi-dev libssl-dev autoconf automake bison swig libglib2.0-dev portaudio19-dev mpg123 screen flac curl libicu-dev pkg-config libjpeg-dev libfann-dev build-essential jq locales
 
+mkdir -p /var/log/mycroft/
+chmod -R 757 /var/log/mycroft/
+
 # git clone https://github.com/MycroftAI/mycroft-core.git /tmp/mycroft --depth=1
 # cd /tmp/mycroft
 # echo '{"use_branch": "master", "auto_update": true}' > .dev_opts.json
